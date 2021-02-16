221659326f00cf7229f94f54f22b8ed6988bff26
claudiubagiu <c_bagiu@yahoo.com>
2021-02-16 11:26:23 +0200

    v2.92

--- PrisonBreak/BackgroundWinScreen.java
+++ PrisonBreak/BackgroundWinScreen.java
@@ -1,20 +1,9 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class BackgroundWinScreen here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;  
 public class BackgroundWinScreen extends NoCollision
 {
-    /**
-     * Act - do whatever the BackgroundWinScreen wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
-        // Add your action code here.
+
     }    
     public BackgroundWinScreen ()
     {
